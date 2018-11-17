4b81d4db6456817cec308713e5981548736e95d7
Nipun <1.madvoid@gmail.com>
2018-11-17 10:12:07 -0700

    PSO Initialization complete

--- PSO.py
+++ PSO.py
@@ -8,9 +8,9 @@
 # -------------------------------------------------------------------------------------------------
 
 import numpy as np
-import scipy as sp
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
+
+
 
 
 class DomainInfo:
@@ -44,6 +44,8 @@ class DomainInfo:
         return f'Domain from {self.minLims} to {self.maxLims} with {self.ds} spacing'
 
 
+
+
 class Particle:
     def __init__(self, domainClass):
         """
@@ -52,31 +54,55 @@ class Particle:
         :param domainClass: DomainInfo instance about current domain
         """
         self.domain = domainClass  # Current domain
-        self.position = np.random.uniform(self.domain.minLims, self.domain.maxLims)  # Initial position
-        self.positionIndex = self.getIndex(self.position)  # Index of initial position
+        self.position = np.random.uniform(self.domain.minLims, self.domain.maxLims)  # Initial position, 1xnDim array
+        self.positionIndex = self.getIndex(
+            self.position)  # Index of initial position, 1xnDim tuple (is a tuple for accessing elements)
         self.velocity = np.random.random_sample(
-            self.domain.dimension) * 0.1  # Initial velocity, multiply by 0.1 so it doesn't explode
-        self.pBestLoc = self.position  # Best location of particle
+            self.domain.dimension) * 0.1  # Initial velocity, multiply by 0.1 so no explode, same size as position
+        self.pBestPos = self.position  # Best location of particle
+        self.currentFitness = None     # Current fitness of particle
 
     def getIndex(self, point):
         """
         Get matrix index of a given point in the domain
 
         :param point: Point you need index of in x,y,z coords
-        :return: The index of the input point
+        :return: The index of the input point. This is to be used with the meshgrid matrices, not the 1-dim matrices!
         """
-        return ((point - self.domain.minLims) / self.domain.ds).astype(int)
+        indexRaw = ((point - self.domain.minLims) / self.domain.ds).astype(int)
+        indexRaw[0], indexRaw[1] = indexRaw[1], indexRaw[0]  # Switch first and second spots since x,y need to be flipped when accessing 2D array since x corresponds to columns and y corresponds to rows
+        return tuple(indexRaw)
 
-    def getFitness(self, costArray):
-        pass
+    def updateFitness(self, costArray):
+        """
+        Update fitness of current particle by accessing cost function array
+
+        :param costArray: Cost function array, will be passed in
+        :return: current fitness value in case it's needed outside of class
+        """
+        self.currentFitness = costArray[self.positionIndex]
+        return self.currentFitness
+
+
 
 
 class PSO:
-    def __init__(self, costArray, numberParticles, maximumIterations, domainClass):
-        self.maxIter = maximumIterations
-        self.numParticles = numberParticles
-        self.costArray = costArray
-        self.particles = [Particle(domainClass) for i in range(self.numParticles)]
+    def __init__(self, costArray, domainClass, numberParticles=25, maximumIterations=1000):
+        self.costArray = costArray          # Array of values of cost function
+        self.domain = domainClass           # DomainInfo instance about current domain
+        self.maxIter = maximumIterations    # Maximum number of iterations
+        self.numParticles = numberParticles # Number of particles
+        self.particles = [Particle(domainClass) for _ in range(self.numParticles)]  # Initialize particles
+        self.bestParticle = None            # Don't get best particle yet
+        for particle in self.particles:     # Find cost of all particles
+            particle.updateFitness(self.costArray)
+        self.getBestParticle()              # Find best particle
+
+    def getBestParticle(self):
+        bestParticleIndex = np.argmin([particle.currentFitness for particle in self.particles])
+        self.bestParticle = self.particles[bestParticleIndex]
+
+
 
 
 if __name__ == "__main__":
@@ -112,5 +138,5 @@ if __name__ == "__main__":
     plt.show()
 
     # Initialize PSO Algorithm
-    AckleyPSO = PSO(C, 25, 1000, AckleyDomain)
+    AckleyPSO = PSO(C, AckleyDomain)
     pass
